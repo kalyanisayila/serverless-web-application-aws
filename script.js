83fc544cd558cc1f84b0996e662808f48279986f
kalyanisayila <sayilakalyani01@gmail.com>
2026-07-11 12:32:27 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log("Serverless Web Application Loaded Successfully!");
+
+document.addEventListener("DOMContentLoaded", function () {
+    alert("Welcome to the Serverless Web Application!");
+});
